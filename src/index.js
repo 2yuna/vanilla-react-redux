a0758d4752def7926febb2d244e510603a6b3a9a
2yuna <y.duck10002@gmail.com>
2021-07-16 09:12:33 +0900

    #3 React Redux - Setup

--- src/index.js
+++ src/index.js
@@ -1,87 +1,10 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const form = document.querySelector("form");
-const input = document.querySelector("input");
-const ul = document.querySelector("ul");
-
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-
-// ===========================================================================
-// Action Creators
-// ===========================================================================
-const addToDo = (text) => {
-  return { type: ADD_TODO, text };
-};
-
-const deleteToDo = (id) => {
-  return { type: DELETE_TODO, id };
-};
-
-// ===========================================================================
-// Reducer
-// ===========================================================================
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      const newToDoObj = { text: action.text, id: Date.now() };
-      return [newToDoObj, ...state];
-    case DELETE_TODO:
-      const cleaned = state.filter((toDo) => toDo.id !== action.id);
-      return cleaned;
-    default:
-      return state;
-  }
-};
-
-// ===========================================================================
-// Store
-// ===========================================================================
-
-const store = createStore(reducer);
-store.subscribe(() => console.log(store.getState()));
-
-// ===========================================================================
-// Dispatch
-// ===========================================================================
-
-const dispatchAddToDo = (text) => {
-  store.dispatch(addToDo(text));
-};
-
-const dispatchDeleteToDo = (e) => {
-  const id = parseInt(e.target.parentNode.id);
-  store.dispatch(deleteToDo(id));
-};
-
-// ===========================================================================
-// Function
-// ===========================================================================
-const paintToDos = () => {
-  ul.innerHTML = "";
-
-  const toDos = store.getState();
-  toDos.forEach((toDo) => {
-    const li = document.createElement("li");
-    li.id = toDo.id;
-    li.innerText = toDo.text;
-
-    const btn = document.createElement("button");
-    btn.innerText = "DEL";
-    btn.addEventListener("click", dispatchDeleteToDo);
-    li.appendChild(btn);
-
-    ul.appendChild(li);
-  });
-};
-store.subscribe(paintToDos);
-
-const onSubmit = (e) => {
-  e.preventDefault();
-  const toDo = input.value;
-  input.value = "";
-
-  dispatchAddToDo(toDo);
-};
-
-form.addEventListener("submit", onSubmit);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
---
+++ src/routes/Home.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+
+function Home(props) {
+  const [text, setText] = useState("");
+
+  function onChange(e) {
+    setText(e.target.value);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    setText("");
+  }
+
+  return (
+    <>
+      <h1>To Do</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="text"
+          value={text}
+          onChange={onChange}
+        />
+        <button>Add</button>
+      </form>
+      <ul></ul>
+    </>
+  );
+}
+
+export default Home;
